a5b59f734bf8c4dac6994e3e412014c3c0ae784e
Ivan Kulikov <kulikovis91@mail.ru>
2024-03-10 18:30:14 +0300

    setup script added

--- setup.sh
+++ setup.sh
@@ -4,6 +4,20 @@ rm -rf tmp
 mkdir tmp
 cd tmp
 
-wget --quiet -P /etc/ https://www.pistar.uk/downloads/XLXHosts.txt
+echo 'Installing deps'
+apt install -y ripgrep
+
+echo 'Downloading configs'
 wget --quiet -P /etc/ https://github.com/krot4u/Public_scripts/raw/master/DMRIds.dat
 wget --quiet -P /etc/ https://raw.githubusercontent.com/g4klx/MMDVMHost/master/RSSI/RSSI_GM340_DEIv1.1.dat
+
+echo 'Geting QRA Team XLX server ip:'
+ip=$(host qra-team.online | rg -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+")
+
+echo 'Generate fake XLXHosts.txt'
+rm -f /etc/XLXHosts.txt
+
+echo "# This XLXHosts.txt is fake and contains only QRA Team XLX server" >> /etc/XLXHosts.txt
+echo "496;$ip;4001" >> /etc/XLXHosts.txt
+
+systemctl restart dmrgateway
