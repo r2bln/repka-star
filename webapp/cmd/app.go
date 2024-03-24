f635abde20cd476f55257b617a57499e79dcdd69
Ivan Kulikov <kulikovis91@gmail.com>
2024-03-24 16:08:28 +0300

    convert ini to json

--- webapp/cmd/app.go
+++ webapp/cmd/app.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"gopkg.in/ini.v1"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	file, _ := os.ReadFile("/home/ivan/sources/repka-star/mmdvmhost.service")
-	fmt.Fprint(w, string(file))
+	cfg, err := ini.Load("/home/ivan/sources/repka-star/mmdvmhost.cfg")
+
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+
+	section, _ := cfg.GetSection(r.URL.Path[1:])
+
+	data := map[string]any{}
+	for key, val := range section.KeysHash() {
+		fmt.Printf("%s - %s\r\n", key, val)
+		data[key] = val
+	}
+
+	print(data)
+
+	js, err := json.Marshal(data)
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	fmt.Fprint(w, string(js))
 }
 
 func main() {
--- webapp/go.mod
+++ webapp/go.mod
@@ -1,3 +1,5 @@
 module repkastar.ru/m
 
 go 1.21.3
+
+require gopkg.in/ini.v1 v1.67.0 // indirect
